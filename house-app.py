fe092bb5c40cfd292de5c26cad205de175cdb34e
Scott <scott@iMac.local>
2018-03-13 06:08:37 -0600

    added graph

--- house-app.py
+++ house-app.py
@@ -5,6 +5,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_table_experiments as dt
 import pandas as pd
+import plotly.graph_objs as go
 
 
 app = dash.Dash()
@@ -39,8 +40,11 @@ app.layout = html.Div(children=[
         # rows=df.to_dict('records'),
         filterable=True,
         sortable=True
-    )
+    ),
+    dcc.Graph(id='graph')
 ])
+
+# update table on change of all dropdowns
 @app.callback(
     Output('datatable', 'rows'),
     [Input('state-choice', 'value'),
@@ -57,6 +61,7 @@ def state_cb(state, metro, zipcode):
         currDF = df[df['State']== state]
         return currDF.to_dict('records')
 
+# update metro dropdown on state dropdown choice
 @app.callback(
     Output('metro-choice', 'options'),
     [dash.dependencies.Input('state-choice', 'value')])
@@ -67,6 +72,7 @@ def metro_cb(dropdown_value):
     metros = sorted(metros)
     return [{'label': m, 'value': m} for m in metros]
 
+# Update zipcode dropdown choices on metro change
 @app.callback(
     Output('zip-choice', 'options'),
     [Input('state-choice', 'value'),
@@ -77,6 +83,25 @@ def zip_cb(state, metro):
     zips = currDF.RegionName.unique()
     zips = sorted(zips)
     return [{'label': z, 'value': z} for z in zips]
+
+
+# Create Graph
+@app.callback(
+    Output('graph', 'figure'),
+    [Input('state-choice', 'value'),
+    Input('metro-choice', 'value'),
+    Input('zip-choice', 'value')])
+def update_figure(state, metro, zip):
+    currDF = df[df['State']== state]
+    dfGroup = currDF.groupby('Metro', as_index = False)['2018-01'].mean()
+    print(dfGroup)
+    return {
+        'data': [go.Bar(
+            x=dfGroup['Metro'],
+            y=dfGroup['2018-01'],
+
+        )]
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
